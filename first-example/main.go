a3fb493ce7244429b69a3391420fcf754ee90e97
Renato Fagalde <renato@likwi.com.br>
2026-09-13 21:13:04 -0300

    feat(first-example): usar sync.WaitGroup para sincronizar múltiplas goroutines

--- first-example/main.go
+++ first-example/main.go
@@ -2,21 +2,26 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func printSomething(s string) {
+func printSomething(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	fmt.Println(s)
 }
 
 func main() {
-	// go printSomething("example 01")
-	// o
+	var wg sync.WaitGroup
+
 	words := []string{
 		"alpha", "beta", "delta", "gamma", "pi", "zeta", "eta", "theta", "epsilon",
 	}
 
-	time.Sleep(3 * time.Second)
+	wg.Add(len(words))
 
-	printSomething("example 02")
+	for i, x := range words {
+		go printSomething(fmt.Sprintf("%d: %s", i, x), &wg)
+	}
+
+	wg.Wait()
 }
